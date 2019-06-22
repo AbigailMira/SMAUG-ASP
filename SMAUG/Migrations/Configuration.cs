3c7da494e4a0c862148178d4955b7d5d40e8eb70
Abigail Mira <miracrick@gmail.com>
2019-06-22 11:25:15 +0200

    initialisation authentification

--- SMAUG/Migrations/Configuration.cs
+++ SMAUG/Migrations/Configuration.cs
@@ -9,7 +9,7 @@ namespace SMAUG.Migrations
     {
         public Configuration()
         {
-            AutomaticMigrationsEnabled = false;
+            AutomaticMigrationsEnabled = true;
         }
 
         protected override void Seed(SMAUG.Models.ApplicationDbContext context)
